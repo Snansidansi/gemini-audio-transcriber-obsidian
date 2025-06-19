aee69aabbde29e5e851bba1faf4b9a14e2bda784
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-06-19 15:42:42 +0200

    Added a stopwatch

---
+++ src/stopwatch.ts
@@ -0,0 +1,51 @@
+import { Notice } from "obsidian";
+
+// eslint-disable-next-line no-unused-vars
+type timeCallback = (formattedTime: string) => void;
+
+export class Stopwatch {
+    private timeInSec = 0;
+    private callback: timeCallback;
+    private intervallID: NodeJS.Timer | null;
+
+    constructor(callback: timeCallback) {
+        this.callback = callback;
+    }
+
+    start() {
+        this.intervallID ??= setInterval(() => {
+            this.timeInSec++;
+            this.callback(this.formatTime());
+        }, 1000);
+    }
+
+    stop() {
+        if (!this.intervallID) {
+            return;
+        }
+
+        clearInterval(this.intervallID);
+        this.intervallID = null;
+    }
+
+    /**
+     * Stops the stopwatch if it is running
+     */
+    reset() {
+        this.stop();
+        this.timeInSec = 0;
+        this.callback(this.formatTime());
+    }
+
+    formatTime(): string {
+        const seconds = this.timeInSec % 60;
+        const minutes = Math.floor((this.timeInSec % 3600) / 60);
+        const hours = Math.floor(this.timeInSec / 3600);
+
+        const secondsFormatted = seconds.toString().padStart(2, "0");
+        const minuesFormatted = minutes.toString().padStart(2, "0");
+        const hoursFormatted = hours.toString().padStart(2, "0");
+
+        return `${hoursFormatted}:${minuesFormatted}:${secondsFormatted}`;
+    }
+}
